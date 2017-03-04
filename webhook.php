db10cc7a6a37ad4545559cea1223d1fdbe0faf92
f4nt0dev <massimo.fantoni2@gmail.com>
2017-03-04 16:15:50 +0100

    time

--- webhook.php
+++ webhook.php
@@ -18,9 +18,13 @@ function processRequest($text)
 	$text = trim($text);
 	$text = strtolower($text);
 	$response = "";
-    $ciao= "ciao";
-    if(strpos($text,$ciao)) {
-      $response = "Ciao! Per ora sono un pappagallo.";
+   
+    if(preg_match('[time|current time|now]', strtolower($text))) {
+      ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $response = $result;
+    }
     }
     else {
         $response = $text;
